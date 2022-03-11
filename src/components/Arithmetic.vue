b5528bc70807e010fc026ca7cc7d600559982049
lizilong <2791778524@qq.com>
2022-03-11 15:41:10 +0800

    算法

--- src/components/Arithmetic.vue
+++ src/components/Arithmetic.vue
@@ -16,6 +16,8 @@ export default {
 
     // console.log(this.getArrayDuplicate([1,2,1,5,6,3,5,3,1,2,6,8,76,9]));
     // this.getTimer();
+    // console.log(this.getFibonacci(10));
+    console.log(this.binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9], 4));
   },
   methods: {
     //冒泡排序降序
@@ -200,8 +202,36 @@ export default {
         if (s < 10) {
           s = `0${s}`;
         }
-        console.log(`${y}年${m}月${d}日： ${h}:${min}:${s}`);
+        return `${y}年${m}月${d}日： ${h}:${min}:${s}`;
       }, 1000);
+    },
+    //生成斐波那契数列
+    getFibonacci(n) {
+      let resArray = [0, 1];
+      let f1 = 0;
+      let f2 = 1;
+      let i = 2;
+      while (i < n) {
+        resArray.push(resArray[i - 1] + resArray[i - 2]);
+        i++;
+      }
+      return resArray;
+    },
+    //二分查找
+    binary_search(arr, key) {
+      let low = 0;
+      let high = arr.length - 1;
+      while (low <= high) {
+        let mid = parseInt((low + high) / 2);
+        if (key === arr[mid]) {
+          return mid;
+        } else if (key < arr[mid]) {
+          high = mid - 1;
+        } else if (key > arr[mid]) {
+          low = mid + 1;
+        }
+      }
+      return -1;
     },
   },
 };
